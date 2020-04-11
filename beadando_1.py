81bc88f1ce3b4cbcf3781b0d2661da6986ec3d25
somak01 <dbksomak@gmail.com>
2020-04-11 10:55:34 +0200

    asd

--- beadando_1.py
+++ beadando_1.py
@@ -1,1 +1,17 @@
-print('asd)')
+import string
+
+def game(num):
+    player1 = 0
+    player2 = 0
+    while num>0:
+        points = input('Number between 1-1000: ')
+        point1,point2 = points.split('')
+        player1 +=point1
+        player2 +=point2
+
+
+
+
+
+rounds = int(input())
+print(game(rounds))
