15699b1f08244472256bf476732ba6da5f6efb54
somak01 <dbksomak@gmail.com>
2020-04-11 11:12:37 +0200

    yee haw

--- beadando_1.py
+++ beadando_1.py
@@ -4,14 +4,26 @@ def game(num):
     player1 = 0
     player2 = 0
     while num>0:
-        points = input('Number between 1-1000: ')
-        point1,point2 = points.split('')
-        player1 +=point1
-        player2 +=point2
+        points = input('2 szam  1-1000 kozott: ')
+        point1,point2 = points.split(' ')
+        player1 +=int(point1)
+        player2 +=int(point2)
+        if player1>player2:
+            print('player1 vezet {} ponttal'.format(player1-player2))
+        elif player2>player1:
+            print('player2 vezet {} ponttal'.format(player2 - player1))
+        else:
+            print("az allas dontetlen")
+        num-=1
+    if player1>player2:
+        print('player1 nyert {} ponttal'.format(player1-player2))
+    elif player2>player1:
+        print('player2 nyert {} ponttal'.format(player2-player1))
+    else:
+        print('dontetlen lett a meccs')
 
 
 
 
-
-rounds = int(input())
-print(game(rounds))
+rounds = int(input('add meg a korok szamat: '))
+game(rounds)
