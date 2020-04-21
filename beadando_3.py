2ebc83023050bc0e568443c7c7c3faa8191e4d53
somak01 <dbksomak@gmail.com>
2020-04-21 09:01:11 +0200

    time

--- beadando_3.py
+++ beadando_3.py
@@ -1,23 +1,23 @@
 list = [7,10,7,0,9,11,0,17,]
 # list = [69,40,0,0,90,0,1]
+# list = [0,0,0,0,0,0,0,0,1]
 def special_sort(list):
     atlag = sum(list)/len(list)
     smh = 0
     seged = []
     # list=[tag for tag in list if (tag>atlag or tag==0)]
     for elem in list:
-        if elem==0:
-            smh+=1
-        elif elem<atlag:
+        if elem<atlag:
             seged.append(elem)
     list.sort()
+    print(seged)
     for tag in seged:
-        list.remove(tag)
+        if tag==0:
+            list.remove(tag)
+            list.append(tag)
+        else:
+            list.remove(tag)
 
-    while smh>0:
-        list.remove(0)
-        list.append(0)
-        smh-=1
 
 special_sort(list)
 print(list)
