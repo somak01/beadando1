ed1782d9a0c32e0cb1e1b26660ad837fd5240ec6
somak01 <dbksomak@gmail.com>
2020-04-21 08:44:23 +0200

    final

--- beadando_1.py
+++ beadando_1.py
@@ -3,8 +3,9 @@ import string
 def game(num):
     player1 = 0
     player2 = 0
-    while num>0:
+    while True:
         try:
+            num -= 1
             points = input('2 szam  1-1000 kozott: ')
             point1,point2 = points.split(' ')
             if int(point1)>1000 or int(point1)<1 or int(point2)<1 or int(point2)>1000:
@@ -12,13 +13,14 @@ def game(num):
                 continue
             player1 +=int(point1)
             player2 +=int(point2)
+            if num==0:
+                break
             if player1>player2:
                 print('player1 vezet {} ponttal'.format(player1-player2))
             elif player2>player1:
                 print('player2 vezet {} ponttal'.format(player2 - player1))
             else:
                 print("az allas dontetlen")
-            num-=1
         except ValueError:
             print('ervenytelen input')
             continue
