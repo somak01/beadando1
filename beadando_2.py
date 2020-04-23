01055649ebb386d469cf3b8ea5ba8d4043934d73
somak01 <dbksomak@gmail.com>
2020-04-23 17:00:42 +0200

    kek

--- beadando_2.py
+++ beadando_2.py
@@ -1,7 +1,21 @@
-str = 'AAAAAABBBBCCCCAA'
-def decode(kod):
-    de = {}
-    flag = kod[1]
-    for ch in kod:
+valami = 'AAAAAABBBBCCCCAA'
 
-
+def code(szo):
+    new_str = ''
+    db = 0
+    tmp = szo[0]
+    for i in range(0, len(szo)):
+        if szo[i]==tmp:
+            db+=1
+        else:
+            new_str+=str(db)+tmp
+            db=0
+            tmp=szo[i]
+            db+=1
+    new_str+=str(db)+tmp
+    return new_str
+def code_to_dict(code):
+    list = []
+    for i in range(len(code)):
+        list.append(code[i:i+1])
+    return list
