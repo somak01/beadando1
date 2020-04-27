85832b10f4a27c2ddc623493b7cbf87fe0788d44
somak01 <dbksomak@gmail.com>
2020-04-27 14:13:07 +0200

    tuturu

--- beadando_2.py
+++ beadando_2.py
@@ -1,26 +1,41 @@
-valami = 'AAAABBBCCDAAA'
+valami = 'AABBCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC'
 
-def code(szo):
-    new_str = ''
-    db = 0
-    tmp = szo[0]
-    for i in range(0, len(szo)):
-        if szo[i]==tmp:
-            db+=1
+def code_to_dict(szo):
+    try:
+        dictionary = {}
+        seged = [szo[0]]
+        db = 0
+        tmp = szo[0]
+        for i in range(0, len(szo)):
+            if szo[i]==tmp:
+                db+=1
+            else:
+                seged.append(szo[i])
+                if tmp not in dictionary:
+                    dictionary[tmp] = [str(db)]
+                    db=0
+                    tmp=szo[i]
+                    db+=1
+                else:
+                    dictionary[tmp].append(str(db))
+                    db = 0
+                    tmp = szo[i]
+                    db+=1
+        if tmp not in dictionary:
+            dictionary[tmp]=[str(db)]
         else:
-            new_str+=str(db)+tmp
-            db=0
-            tmp=szo[i]
-            db+=1
-    new_str+=str(db)+tmp
-    return new_str
-def code_to_dict(code):
-    list_values = []
-    list_keys =[]
-    for i in range(1,len(code), 2):
-        list_keys.append('part'+str((i+1)//2))
-        list_values.append(code[i-1:i+1])
-    dictionary =dict(zip(list_keys, list_values))
-    print(dictionary)
-code_to_dict(code(valami))
+            dictionary[tmp].append(str(db))
+        kimenet = ''
+        for ch in seged:
+            if ch not in kimenet:
+                kimenet+=ch+dictionary[ch][0]
+            else:
+                kimenet+=ch+dictionary[ch][kimenet.count(ch)]
+        print(kimenet)
+        print(dictionary)
+        print(seged)
+
+    except IndexError:
+        print('Letezzen a string')
+code_to_dict(valami)
 
