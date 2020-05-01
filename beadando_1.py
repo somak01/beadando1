7ba766b595fca980e93a6c3d77d39a07da16616d
somak01 <dbksomak@gmail.com>
2020-05-01 10:41:28 +0200

    final

--- beadando_1.py
+++ beadando_1.py
@@ -5,15 +5,15 @@ def game(num):
     player2 = 0
     while True:
         try:
-            num -= 1
-            points = input('2 szam  1-1000 kozott: ')
+            points = input('Add meg a pontszamokat (2 szam  1-1000 kozott szokozzel elvalasztva): ')
             point1,point2 = points.split(' ')
             if int(point1)>1000 or int(point1)<1 or int(point2)<1 or int(point2)>1000:
                 print('ervenytelen szam')
                 continue
             player1 +=int(point1)
             player2 +=int(point2)
-            if num==0:
+            num -= 1
+            if num == 0:
                 break
             if player1>player2:
                 print('player1 vezet {} ponttal'.format(player1-player2))
@@ -23,16 +23,18 @@ def game(num):
                 print("az allas dontetlen")
         except ValueError:
             print('ervenytelen input')
-            continue
+    print('a vegeredmeny: Player1 {} - {} Player2'.format(player1, player2))
     if player1>player2:
-        print('player1 nyert {} ponttal'.format(player1-player2))
+        print('Player1 nyert {} ponttal '.format(player1-player2))
     elif player2>player1:
-        print('player2 nyert {} ponttal'.format(player2-player1))
+        print('Player2 nyert {} ponttal '.format(player2-player1))
     else:
         print('dontetlen lett a meccs')
 
-
-
-
-rounds = int(input('add meg a korok szamat: '))
-game(rounds)
+while True:
+    try:
+        rounds = int(input('add meg a korok szamat:' ))
+        game(rounds)
+        break
+    except ValueError:
+        print('hibas input')
