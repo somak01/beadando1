b7122c49786093ab11e5595ff02a4f730d05746b
somak01 <dbksomak@gmail.com>
2020-05-11 15:10:54 +0200

    final

--- beadando_37.py
+++ beadando_37.py
@@ -1,5 +1,6 @@
-list = [7,10,7,0,9,11,0,17,15,16,12]
-# list = [69,40,0,0,90,0,1]
+# list = [7,10,7,0,9,11,0,17,15,16,12]
+# list = [68,40,0,0,90,0,1]
+list = [7,10,0,9,11,0,17]
 # list = [0,0,0,0,0,0,0,0,1]
 # list = []
 def sorting(list):
@@ -24,6 +25,6 @@ def special_sort(list):
         else:
             list.remove(tag)
 
-
+# print(list)
 special_sort(list)
 print(list)
